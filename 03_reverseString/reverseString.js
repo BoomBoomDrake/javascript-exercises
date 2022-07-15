c68df3c7ef3ba14efaf0536ec1f896a052bb86a6
eric <eric.drake2011@gmail.com>
2022-07-14 23:00:13 -0500

    Complete exercise .js file reverses strings and .skip removed from tests in .spec.js files

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let ch = Array.from(str);
+    let revArr = [];
+    console.log('ch', ch);
+    
+    for (i = 0; i < str.length; i++) {
+        revArr[i] = str.charAt((str.length - 1) - i);
+    }
 
+    console.log('revArr', revArr)
+
+    let result = revArr.join('');
+    console.log('result', result);
+
+    return result
 };
 
 // Do not edit below this line
