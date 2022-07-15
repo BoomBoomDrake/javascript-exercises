b27e2ead1acd6507fafb544a3f503fc5edba2505
eric <eric.drake2011@gmail.com>
2022-07-14 20:46:29 -0500

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = [];
 
+    if (num < 0) {
+        result = 'ERROR';
+        
+        return result;
+    }
+
+    for (i = result.length; i < num; i++){
+        result.push(string);
+    }
+
+    return result.join('');
 };
 
 // Do not edit below this line
