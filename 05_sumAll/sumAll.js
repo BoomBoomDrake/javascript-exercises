024977f61517c9ac415da255d190f3cd56a2320b
eric <eric.drake2011@gmail.com>
2022-07-22 16:48:55 -0500

    Add post solution comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,11 @@
 const sumAll = function(a, b) {
-    let big = (a > b) ? a : b;
+    let big = (a > b) ? a : b; // alternative: if (min > max) { const temp = min; min = max; max = temp;} switcharoo!
     let small = (a > b) ? b : a;
     let result = 0;
     const error = 'ERROR';
 
     if ( a < 0 || b < 0) return error;
-    if (typeof a !== 'number' || typeof b !== 'number') return error;
+    if (typeof a !== 'number' || typeof b !== 'number') return error; // alternative: if (!Number.isInteger(a) || !Number.isInteger(b))
 
     for (i = small; i <= big; i++) {
         result += i;
