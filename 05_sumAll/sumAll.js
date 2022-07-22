de5712727f5e380a09ecafb06a2561df9e77436e
eric <eric.drake2011@gmail.com>
2022-07-22 15:47:19 -0500

    Removed .skips and solved exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let big = (a > b) ? a : b;
+    let small = (a > b) ? b : a;
+    let result = 0;
+    const error = 'ERROR';
 
-};
+    if ( a < 0 || b < 0) return error;
+    if (typeof a !== 'number' || typeof b !== 'number') return error;
+
+    for (i = small; i <= big; i++) {
+        result += i;
+    }
+
+    return result;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
