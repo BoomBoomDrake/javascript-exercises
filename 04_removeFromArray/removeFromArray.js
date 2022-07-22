03c3097e4a7ed94e0e997fc709e40e6620d8cda9
eric <eric.drake2011@gmail.com>
2022-07-22 11:32:43 -0500

    Removed .skip from tests and solved exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+let strArr = [];
 
-};
+const removeFromArray = function(strArr, ...args) {
+
+    args.forEach(x => {
+        let index = strArr.indexOf(x);
+
+        if (index > -1) {
+            strArr.splice(index, 1);
+        }
+    })
+    
+    return strArr;
+}
+//
+    //args.forEach( str => matchMap[str] = true);
+    //console.log(matchMap);
+//
+    //strArr.forEach( str => {
+    //  if (!matchMap[str]) {
+    //    resultArr.push(str);
+    //  }
+    //})
+//
+    //return resultArr;
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
