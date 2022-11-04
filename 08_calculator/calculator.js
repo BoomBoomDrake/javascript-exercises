31cd0134ef18a37eb023743025759e4b7c45d006
eric <eric.drake2011@gmail.com>
2022-11-04 16:34:55 -0500

    complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(...args) {
+	let total = 0;
+  args.forEach((num) => {
+    total += num;
+  })
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let total = args[0];
+  for (let i = 1; i < args.length; i++) {
+    total -= args[i];
+  }
+  
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  arr.forEach((num) => {
+    total += num;
+  })
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.forEach((num) => {
+    total *= num;
+  })
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return (num === 0 || num === 1) ? 1 : num * factorial(num - 1);
 };
 
 // Do not edit below this line
