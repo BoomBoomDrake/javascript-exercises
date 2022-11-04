f921dc0969752200642fa3d9d65d0c182aa1566b
eric <eric.drake2011@gmail.com>
2022-11-04 17:20:57 -0500

    complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let alpha = /[\W_]/g;
+    let lowReg = str.toLowerCase().replace(alpha, '');
+    let reverse = lowReg.split('').reverse().join('');
+    return reverse === lowReg;
 };
 
 // Do not edit below this line
