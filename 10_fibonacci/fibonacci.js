c047c53efa557a8e80925f89b826e4860d312318
eric <eric.drake2011@gmail.com>
2022-11-04 17:47:43 -0500

    complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArr = [0, 1];
+    
+    if (num < 1 || isNaN(num)) {
+        return 'OOPS';
+    }
+    for (let i = 1; i < Number(num); i++) {
+        let a = fibArr[i]
+        let b = fibArr[i - 1];
+        let c = a + b;
+        fibArr.push(c);
+    }
+    return fibArr[fibArr.length - 1];
 };
 
 // Do not edit below this line
