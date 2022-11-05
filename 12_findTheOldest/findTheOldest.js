ced3bdb2c510f5e2a7453ff0756a5211dd4acf2c
eric <eric.drake2011@gmail.com>
2022-11-05 09:18:09 -0500

    complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
 
+    for (let i = 1; i < people.length; i++) {
+        if (calcAge(oldest) < calcAge(people[i])) {
+            oldest = people[i];
+        }
+    }
+
+    return oldest;
 };
+
+function calcAge(obj) {
+    if (!(obj.yearOfDeath)) {
+        let year = new Date().getFullYear();
+        return year - obj.yearOfBirth;
+    }
+
+    return obj.yearOfDeath - obj.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
